65fb867b77bd0c52002631049b70e1a1b087910f
Carolina Ribeiro <carolinaribeirodacol@gmail.com>
2026-06-29 00:47:47 -0300

    feat: adicionado imagem og

--- app/app.vue
+++ app/app.vue
@@ -6,7 +6,7 @@ useSeoMeta({
   ogTitle: 'My Amazing Site',
   description: 'This is my amazing site, let me tell you all about it.',
   ogDescription: 'This is my amazing site, let me tell you all about it.',
-  ogImage: 'https://example.com/image.png',
+  ogImage: '/project-image.png',
   twitterCard: 'summary_large_image',
 })
 </script>
